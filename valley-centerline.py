741d55662f618ac61278634aa48bf675e9569d90
Peter Mitchell <pmitchell490@icloud.com>
2021-03-18 13:04:49 -0500

    Bounding box -> buffer, centerline extraction

--- valley-centerline.py
+++ valley-centerline.py
@@ -2,6 +2,8 @@ import shapely
 import geopandas as gpd
 from geovoronoi import voronoi_regions_from_coords
 
+BUFFER_DISTANCE = 0.0001
+
 # Import line shapefiles
 valleywall_01 = gpd.read_file("C:\LSDTopoTools\Github\Valley-Centerline\Wall2.shp")
 valleywall_02 = gpd.read_file("C:\LSDTopoTools\Github\Valley-Centerline\Wall1.shp")
@@ -10,77 +12,114 @@ valleywallshape_01 = shapely.geometry.shape(valleywall_01['geometry'][0])
 valleywallshape_02 = shapely.geometry.shape(valleywall_02['geometry'][0])
 
 # Convert valley walls into a collection of points for voronoi algorithm
-coords = valleywallshape_01.coords[:] + valleywallshape_02.coords[:]
-points = shapely.geometry.MultiPoint(coords)
-print(points)
 
-# Generate bounding box around valley walls for voronoi algorithm
-x_min = None
-x_max = None
-y_min = None
-y_max = None
-for coord in coords:
-    if x_min == None or coord[0] < x_min:
-        x_min = coord[0]
-    if x_max == None or coord[0] > x_max:
-        x_max = coord[0]
-    if y_min == None or coord[1] < y_min:
-        y_min = coord[1]
-    if y_max == None or coord[1] < y_max:
-        y_max = coord[1]
-boundingbox = shapely.geometry.box(x_min - 10, y_min - 10, x_max + 10, y_max + 10)
+# This bit assumes both lines were drawn in the same direction
+# @TODO: make it so it works when lines are drawn in opposite directions
+coords = valleywallshape_01.coords[:] + valleywallshape_02.coords[::-1]
+start_boundary = shapely.geometry.LineString([valleywallshape_01.coords[0], valleywallshape_02.coords[0]])
+end_boundary = shapely.geometry.LineString([valleywallshape_01.coords[-1], valleywallshape_02.coords[-1]])
+
+points = shapely.geometry.MultiPoint(coords)
+
+# Generate polygon and buffer from the input walls
+valleypoly = shapely.geometry.Polygon(coords)
+buffer = valleypoly.buffer(BUFFER_DISTANCE)
+
+features = [0]
+geometry = [valleypoly]
+df = {'features': features, 'geometry': geometry}
+gdf = gpd.GeoDataFrame(df)
+gdf.to_file('C:\LSDTopoTools\Github\Valley-Centerline\\valleypoly.shp')
+
+features = [0]
+geometry = [buffer]
+df = {'features': features, 'geometry': geometry}
+gdf = gpd.GeoDataFrame(df)
+gdf.to_file('C:\LSDTopoTools\Github\Valley-Centerline\\valleybuffer.shp')
 
 # Create Voronoi Polygons
-region_polys, region_pts = voronoi_regions_from_coords(points, boundingbox)
+region_polys, region_pts = voronoi_regions_from_coords(points, buffer)
 
 #Export the voronoi polygons
+
 features = [i for i in range(len(region_polys))]
 geometry = [geom for geom in region_polys.values()]
 df = {'features': features, 'geometry': geometry}
 gdf = gpd.GeoDataFrame(df)
 gdf.to_file('C:\LSDTopoTools\Github\Valley-Centerline\\voronoi.shp')
 
-# Get necessary voronoi edges
+
 voronoi_edges = []
+
 for poly in region_polys.values():
-    for i in range(len(poly.exterior.coords) - 1):
-        edge = shapely.geometry.LineString((poly.exterior.coords[i], poly.exterior.coords[i+1]))
-        # Get rid of edges that touch the bounding box or cross the valley walls
-        # FIXME - Some edges inside and outside of the walls still slip through
-        if not(edge.crosses(valleywallshape_01) or edge.crosses(valleywallshape_02) or edge.intersects(boundingbox.exterior)):
-            voronoi_edges.append(edge)
-# Export voronoi edges
+    if poly.geom_type == 'MultiPolygon':
+        # If the buffer distance is too small (<0.0006 in this case), some of the voronoi outputs are MultiPolygons.
+        # I'm just discarding them here, which seems to work alright, but I'm wondering if there are cases when this would give a bad result.
+        pass
+    else:
+        for i in range(len(poly.exterior.coords) - 1):
+            edge = shapely.geometry.LineString((poly.exterior.coords[i], poly.exterior.coords[i+1]))
+            repeat = False
+
+            # Edges will have duplicates if they're the border between polygons. If this is the case, we don't want to add the duplicates.
+            for test in voronoi_edges:
+                if edge.coords[:] == test.coords[:]:
+                    # In all the cases so far, it seems that the coordinates have been flipped, so I'm not sure if this will ever be the case.
+                    repeat = True
+                if edge.coords[:] == test.coords[::-1]:
+                    repeat = True
+            # Get rid of edges that are duplicates or outside of the valley
+            if edge.within(valleypoly) and not repeat:
+                voronoi_edges.append(edge)
+            # Get the first and last segments, which should intersect the lines connecting the two valley walls
+            if edge.intersects(start_boundary) and not(edge.intersects(valleywallshape_01) or edge.intersects(valleywallshape_02)):
+                start_edge = edge
+            if edge.intersects(end_boundary) and not(edge.intersects(valleywallshape_01) or edge.intersects(valleywallshape_02)):
+                end_edge = edge
+
+# Export voronoi edges (for testing purposes)
 features = [i for i in range(len(voronoi_edges))]
 geometry = [geom for geom in voronoi_edges]
 df = {'features': features, 'geometry': geometry}
 gdf = gpd.GeoDataFrame(df)
 gdf.to_file('C:\LSDTopoTools\Github\Valley-Centerline\\voronoi_edges.shp')
-# TODO - Connect edges into complete centerline
-# TODO - Export centerline
 
-'''
-# Project points along lines
-midpoints = []
-for point in valleywallshape_01.coords[:]:
-    # Get distance along line
-    project_point = shapely.geometry.Point(point)
-    projection_distance = valleywallshape_02.project(project_point)
-    
-    # Get coordinates of that distance
-    interpolated_point = valleywallshape_02.interpolate(projection_distance)
-    print(interpolated_point)
+tried_edges = []
 
-    # Make a line between that point and the projected point
-    line = shapely.geometry.LineString([interpolated_point, project_point])
-    # Find the halfway point
-    if(not(line.crosses(valleywallshape_01) or line.crosses(valleywallshape_02))):
-        midpoint = line.interpolate(0.5, normalized=True)
-        midpoints.append(midpoint.coords[:][0])
+# Recursive solution for getting only the centerline from beginning to end
+def get_centerline_path(edge, from_edge):
+    viable_edges = []
+    global centerlineedges
+    global tried_edges
+    # Add self to the list of tried edges
+    tried_edges.append(edge)
+    if edge.touches(end_edge):
+        # If we've found the end, add ourselves to the list and let the edge that found us know
+        centerlineedges.append(edge)
+        centerlineedges.append(end_edge)
+        return True
+    # See which neighbors haven't been checked yet
+    for other_edge in voronoi_edges:
+        if edge.touches(other_edge) and other_edge not in tried_edges:
+            # Check if the edge is one of from_edge's viable edges:
+            if from_edge is None or not(from_edge.touches(other_edge)):
+                viable_edges.append(other_edge)
+    for other_edge in viable_edges:
+        if get_centerline_path(other_edge, edge):
+            # If one of our viable edges is on the path toward the end, add ourselves to the list and let the edge that found us know
+            centerlineedges.append(edge)
+            return True
+    return False
 
-midpoint_line = shapely.geometry.LineString(midpoints)
+centerlineedges = []
+get_centerline_path(start_edge, None)
+
+
+# Export centerline
+centerline = shapely.geometry.MultiLineString(centerlineedges)
+centerline_merged = shapely.ops.linemerge(centerline)
 features = [0]
-df = {'features': features, 'geometry': [midpoint_line]}
+geometry = [centerline_merged]
+df = {'features': features, 'geometry': geometry}
 gdf = gpd.GeoDataFrame(df)
-gdf.to_file('C:\LSDTopoTools\Github\Valley-Centerline\Zumbro_centerline_3.shp')
-print(gdf)
-'''
+gdf.to_file('C:\LSDTopoTools\Github\Valley-Centerline\\centerlineedges.shp')
